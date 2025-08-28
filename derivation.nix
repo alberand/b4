1e0334fcd8a7392eb75b078a3ecfa0960baf4a33
Andrey Albershteyn <aalbersh@kernel.org>
2025-08-28 15:37:32 +0200

    Add NixOS packaging
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>

---
+++ derivation.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  python3Packages,
+  fetchPypi,
+  patatt,
+}:
+
+python3Packages.buildPythonApplication rec {
+  pname = "b4";
+  version = "0.14.2";
+  pyproject = true;
+
+  src = ./.;
+
+  # tests make dns requests and fails
+  doCheck = false;
+
+  build-system = with python3Packages; [
+    setuptools
+  ];
+
+  propagatedBuildInputs = with python3Packages; [
+    requests
+    dnspython
+    dkimpy
+    patatt
+    git-filter-repo
+    packaging
+  ];
+
+  meta = with lib; {
+    homepage = "https://git.kernel.org/pub/scm/utils/b4/b4.git/about";
+    license = licenses.gpl2Only;
+    description = "Helper utility to work with patches made available via a public-inbox archive";
+    mainProgram = "b4";
+    maintainers = with maintainers; [
+      jb55
+      qyliss
+      mfrw
+    ];
+  };
+}
